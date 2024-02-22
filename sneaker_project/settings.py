cd6a4d778042ea0658ad1aef43e3b3394359c764
Ekow Yawson <ekowyawson71@gmail.com>
2024-02-21 19:55:12 -0500

    Ensured that admin login works

--- sneaker_project/settings.py
+++ sneaker_project/settings.py
@@ -136,7 +136,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
-    "DEFAULT_AUTHENTICATION_CLASSES": [ # new: lab 33
+    "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
